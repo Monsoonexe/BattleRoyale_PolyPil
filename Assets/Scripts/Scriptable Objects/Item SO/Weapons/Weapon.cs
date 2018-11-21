e0b7d72833c613ca641a075d53a9238e89d0166d
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2018-11-21 11:31:47 -0800

    added Range attribute to hands
    have to use either 1 or 2 hands

--- Assets/Scripts/Scriptable Objects/Item SO/Weapons/Weapon.cs
+++ Assets/Scripts/Scriptable Objects/Item SO/Weapons/Weapon.cs
@@ -4,6 +4,7 @@ public class Weapon : Item {
     [Header("Weapon")]
     public AmmoTypeENUM ammoType;
     public WeaponTypeENUM weaponType;
+    [Range(1, 2)]
     public int hands;
     public GameObject[] attachments;
 
