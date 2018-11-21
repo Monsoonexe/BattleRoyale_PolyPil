fa318d551460d9780cb975ab2bfacf36f01c96a5
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2018-11-21 10:23:39 -0800

    created new colors in Scriptable Object
    set colors

--- Assets/Scripts/Scriptable Objects/Color SO/Colors_Rarity.cs
+++ Assets/Scripts/Scriptable Objects/Color SO/Colors_Rarity.cs
@@ -1,45 +1,45 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine;
 
 public class Colors_Rarity : ScriptableObject
 {
-    [Header("Colors")]
-    public Color color_HealthHigh;
-    public Color color_HealthHalf;
-    public Color color_HealthLow;
-    public Color color_HealthDanger;
-
-    [Header("Limits")]
-    public float limit_HealthHalf = .50f;
-    public float limit_HealthLow = .25f;
-    public float limit_HealthDanger = .10f;
-
-    public void SetHealthColor(float currentHealth, float maxHealth, Text healthText)
+    [Header("Colors for Rarity")]
+    public Color common;
+    public Color uncommon;
+    public Color rare;
+    public Color epic;
+    public Color legendary;
+    public Color mythic;
+    
+    public Color GetColor(ItemRarityENUM rarity)
     {
-        float healthPercent = 1.0f; //default at 100% health (white) 
-        healthPercent = currentHealth / maxHealth; //get percent
-        //Debug.Log("healthPercent: " + healthPercent.ToString());//print test
-        if (healthPercent <= limit_HealthDanger)
+        Color rarityColor;
+        switch (rarity)
         {
-            healthText.color = color_HealthDanger;
+            case ItemRarityENUM.Common:
+                rarityColor = common;
+                break;
+            case ItemRarityENUM.Uncommon:
+                rarityColor = uncommon;
+                break;
+            case ItemRarityENUM.Rare:
+                rarityColor = rare;
+                break;
+            case ItemRarityENUM.Epic:
+                rarityColor = epic;
+                break;
+            case ItemRarityENUM.Legendary:
+                rarityColor = legendary;
+                break;
+            case ItemRarityENUM.Mythic:
+                rarityColor = mythic;
+                break;
+            default:
+                Debug.Log("What am I doing with my life?");
+                rarityColor = Color.white;
+                break;
         }
-        else if (healthPercent < limit_HealthLow)
-        {
-            healthText.color = color_HealthLow;
-        }
-        else if (healthPercent < limit_HealthHalf)
-        {
-            healthText.color = color_HealthHalf;
-        }
-        else
-        {
-            healthText.color = color_HealthHigh;
-            healthText.text = currentHealth.ToString();
-            //TODO get a color between white and yellow this percent
-        }
-
+        return rarityColor;
     }
+    
 
 }
