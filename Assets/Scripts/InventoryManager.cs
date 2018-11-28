ed23debb2561398a93102c636378893a31f4f93a
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2018-11-27 22:02:52 -0800

    Update InventoryManager.cs
    integrating adding currency, items

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -9,7 +9,7 @@ public class InventoryManager : MonoBehaviour {
 
     public int cashOnHand = 0;
     public int inventorySlots = 3;
-    public List<Item> inventoryArray = new List<Item>();
+    public List<InventorySlot> inventoryArray = new List<InventorySlot>();
 
     [Header("Resources")]
 
@@ -36,21 +36,27 @@ public class InventoryManager : MonoBehaviour {
 
     //}
 
-public void ModifyMoney(int amount)
+public bool ModifyMoney(int amount)
     {
+        //TODO Implement different kinds of currency
+        bool operationSuccessful = true;//prove me wrong
         cashOnHand += amount;
         if (DEBUG) Debug.Log("Wallet Balance: " + cashOnHand);
         if(cashOnHand < 0)
         {
-            Debug.LogError("ERROR! Too much money spent when not enough had!");
+            Debug.LogError("ERROR! Too much money spent when not enough had! Wallet contains $" + cashOnHand);
+            operationSuccessful = false;//wrong!
         }
+
+        return operationSuccessful;//returns whether or not the operation was successful
     }
 
-    public void ModifyResources(Resource resource)
+    public bool ModifyResources(Resource resource, int quantity = 1)
     {
-        //TODO
-        //Resource dictionary
+        bool operationSuccessful = true;//prove me wrong
+        //TODO Resource dictionary
 
+        return operationSuccessful;
     }
 
     //is this item stackable?
@@ -58,61 +64,119 @@ public void ModifyMoney(int amount)
     //finish stack, then add rest
     //is there enough space?
     //add
+    private bool AddToNextAvailableSlot(Item item, int quantity = 1)
+    {
+        //returns the number of items added
+        bool operationSuccessful = false;
+        if (inventoryArray.Count >= inventorySlots) return false;//inventory full
+        for(int i = 0; i < inventoryArray.Count; ++i)
+        {
+            if(inventoryArray[i] == null)
+            {
+                inventoryArray[i] = new InventorySlot(item, quantity, item.stackQuantity);
+                operationSuccessful = true;
+                break;
+            }
+        }
+        return operationSuccessful;
+    }
+
+    private List<int> FindIndicesOfItem(Item itemToFind)
+    {//returns every index at which the item is kept in a slot
+        List<int> indicesOfItem = new List<int>();
+        for(int i = 0; i < inventoryArray.Count; ++i)
+        {
+            if(inventoryArray[i].GetItem() == itemToFind)
+            {
+                indicesOfItem.Add(i);
+            }
+        }
+        return indicesOfItem;
+    }
+
+    private int AddQuantityToSlot(int quantity, int index)
+    {
+        //returns leftovers, if any
+        return inventoryArray[index].AddQuantity(quantity);
+        
+    }
+
+    private int AddStackableInventory(Item itemToAdd, int quantity)
+    {   
+        //does this item already exist in inventory? try to stack
+        List<int> indicesOfExistingItem = FindIndicesOfItem(itemToAdd);
+        if(indicesOfExistingItem.Count > 0)//if this item does exist
+        {
+            for(int i = 0; i < indicesOfExistingItem.Count;  ++i)
+            {
+                //subtract the amount that was given back by AddQuantity()
+                quantity = AddQuantityToSlot(quantity, i);
+                if (quantity <= 0) return 0;//if quantity > 0, the existing stack was not big enough to accept full quantity. look for more
+
+            }
+        }
+
+        //if there's room, add the rest to a new slot
+        if (inventoryArray.Count < inventorySlots) AddToNextAvailableSlot(itemToAdd, quantity);
+               
+        return quantity;
+    }//end AddStackableInventory()
 
     public bool AddItem(FN_ItemManager itemManager)
     {
         Item item = itemManager.scriptableObject_Item;
-        bool canAddItem = false;
+        bool itemAddSuccess = false;
 
-        if(item is Ammo)
+        
+
+
+        //if(item is Ammo)
+        //{
+        //}
+        //else if (item is Attachment)
+        //{
+        //}
+        if (item is Currency)
         {
-            //does this item already exist in inventory?
-            //is there room in that slot for this amount?
-            if (inventoryArray.Count < inventorySlots)
-            {
-                //
-                canAddItem = true;
-            }
-        }
-        else if (item is Attachment)
-        {
-            if (inventoryArray.Count < inventorySlots)
-            {
-                //
-                canAddItem = true;
-            }
-        }
-        else if (item is Currency)
-        {
-            ModifyMoney(itemManager.ItemAmount);
-            canAddItem = true;
+            //TODO currency dictionary
+            itemAddSuccess = ModifyMoney(itemManager.ItemAmount);
         }
         else if (item is Resource)
         {
-            ModifyResources((Resource)item);
-            canAddItem = true;
+            //TODO Resource dictionary
+            itemAddSuccess = ModifyResources(item as Resource);
+            
         }
-        else if (item is Throwable)
+        //else if (item is Throwable)
+        //{
+
+        //}
+        //else if(item is Weapon)
+        //{
+        //    //done
+        //    if (inventoryArray.Count < inventorySlots)
+        //    {
+        //        itemAddSuccess = AddToNextAvailableSlot(item);
+        //    }
+        //}
+        else
         {
-            //does this item already exist in inventory?
-            //is there room in that slot for this amount?
-            if (inventoryArray.Count < inventorySlots)
+
+            if (item.stackQuantity <= 1)
             {
-                //
-                canAddItem = true;
+                itemAddSuccess = AddToNextAvailableSlot(item);
             }
-        }
-        else if(item is Weapon)
-        {
-            if (inventoryArray.Count < inventorySlots)
+            else
             {
-                //
-                canAddItem = true;
+                int quantityRemaining = AddStackableInventory(item, itemManager.ItemAmount);
+                itemManager.ItemAmount = quantityRemaining;
+                itemAddSuccess = false;//redundant
+                
             }
         }
 
         //return whether or not the item was accepted
-        return canAddItem;
+        return itemAddSuccess;
     }//end AddItem()
 
    
